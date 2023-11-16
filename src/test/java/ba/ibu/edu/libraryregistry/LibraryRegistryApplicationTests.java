498e487ab8d55a3ac7beffea407dad0309680775
Aldin Kovačević <aldin@salesseek.com>
2023-11-16 09:07:22 +0100

    Initial commit.

---
+++ src/test/java/ba/ibu/edu/libraryregistry/LibraryRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package ba.ibu.edu.libraryregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
